81f49ed14b800cdaf0b7acfcb9edac018365f0c4
Shoaib72 <shoaib.953872@gmail.com>
2022-11-09 17:19:25 +0000

    server-created

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Sky Hype Server is Running!')
+})
+app.listen(port, () => {
+    console.log(`Sky Hype Server Is Running On Port: ${port}`)
+})
