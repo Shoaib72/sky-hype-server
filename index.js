328b6f09effc91ffca92574a0f12f1bb06b061d8
Shoaib72 <shoaib.953872@gmail.com>
2022-11-10 00:30:02 +0000

    home route created

--- index.js
+++ index.js
@@ -12,13 +12,24 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lp2erlk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+console.log(uri)
 
+async function run() {
+    try {
+        const serviceCollection = client.db('skyHype').collection('services');
+        app.get('/home', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally {
 
+    }
+}
+
+run().catch(err => console.error(err))
 
 app.get('/', (req, res) => {
     res.send('Sky Hype Server is Running!');
